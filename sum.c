dfe76b422ccc8c6f4eaadca94b13b125fe01f32a
abdhulla-k <abdhullak149@gmail.com>
2022-04-24 20:33:18 +0530

    created a functon to calculate sum

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+
+    int num1, num2;
+
+    printf("Enter two number: ");
+    scanf("%d%d", &num1, &num2);
+    int result = num1+num2;
+    printf("sum is %d",result);
+
+}
