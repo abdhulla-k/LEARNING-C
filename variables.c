3552c13fecf71cf120ea74f68fc85d464d2d3630
abdhulla-k <abdhullak149@gmail.com>
2022-04-24 19:44:01 +0530

    learned about variables in c

---
+++ variables.c
@@ -0,0 +1,17 @@
+// variable declaration 
+// ther are many variable type:
+// intiger(int), float, string(char)
+// use these keywords before declaration
+// eg.
+
+#include <stdio.h>
+int main() {
+    int variable1 = 3; // data must be an intiger incase of int
+    float variable2 = 12.12345; // data must be a float value
+    char variable3 = 'A'; // data must be a string incase of char
+
+    // lets try to prit all variables
+    printf( "%d", variable1 );
+    printf( "\n%f", variable2 );
+    printf("\n%c", variable3 );
+}
