dec36ea4780ae47f6efb1efc542156ff5436b759
abdhulla-k <abdhullak149@gmail.com>
2022-05-25 15:58:09 +0530

    completted pattern 4

---
+++ patterns/print_the_pattern/pattern4.c
@@ -0,0 +1,34 @@
+/* 
+*
+* * * *
+*
+*
+* * * * * * * * 
+*
+*
+*
+* * * * * * * * * * * * 
+etc..
+*/
+#include <stdio.h>
+int main() {
+    int n = 10;
+    int i, j;
+    int count1 = 4;
+    float count2 = 1;
+
+    for( i = 1; i <= n; i++ ) {
+        if( i % 2 == 1 ) {
+            for( j = 0; j < count1; j++ ) {
+                printf( "* " );
+            }
+            count1 += 4;
+            printf( "\n" );
+        } else {
+            for( j = 0; j < count2; j++ ) {
+                printf( "*\n" );
+            }
+            count2++;
+        }
+    }
+}
