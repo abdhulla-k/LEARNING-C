a11971b264924399c58bae220e87b378bca709ef
abdhulla-k <abdhullak149@gmail.com>
2022-05-25 13:37:21 +0530

    written a program to print the a star pattern

--- patterns/pattern1.c
+++ patterns/pattern1.c
@@ -1,26 +1,67 @@
-/*
-// we are going to print the pattern:
+/*  print the following pattern;
+        *
 
-*
-**
-***
-****
-*****
-........ n
+      * * *
 
-*/
+    *   *   *
+
+  *     *     *
+
+* * * * * * * * *
+
+  *     *     *
+
+    *   *   *
+
+      * * *
+      
+        *
+ */
 
 #include <stdio.h>
 int main() {
-    int n;
-    printf("Eter a number: ");
-    scanf("%d",&n);
+    int n = 5;
+    int i, j, k;
 
-    // create the algorithm
-    for( int i = 1; i <= n; i++ ) {
-        for( int j = 1; j <= i; j++ ) {
-            printf("*");
+    // prit the pattern
+    for( i = 0; i < n; i++ ) {
+      for( j = 0; j < n - ( i + 1 ); j++ ) {
+        printf( "  " );
+      }
+      if( i != n-1 ) {
+
+        // print the space
+        for( k = j + 1; k <= n + i; k++ ) {
+
+          // print the star
+          if( k == n | k == j + 1 | k == n + i ) {
+            printf( "* " );
+          } else {
+            printf( "  " );
+          }
         }
-        printf("\n");
+      } else {
+        for( k = 0; k < n + i; k++ ) {
+          printf( "* " );
+        }
+      }
+      printf( "\n" );
+      printf( "\n" );
+    }
+
+    for( i = 0; i < n - 1; i++ ) {
+      for( j = 0; j < n + ( n - (i+2)); j++ ) {
+
+        // print the star
+        if( j == i + 1 | j == n - 1 | j == n + (n - ( i + 3 ))) {
+          printf( "* " );
+        } else {
+          
+          // print the space
+          printf( "  " );
+        }
+      }
+      printf( "\n" );
+      printf( "\n" );
     }
 }
