af2a0791937c5784046b01df48443555735d0b8c
abdhulla-k <abdhullak149@gmail.com>
2022-07-21 14:03:43 +0530

    completed first problem in assignment. input a char and print it

---
+++ assignment/accept_char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char charInput;
+
+    printf( "Enter a letter :\t" );
+
+    // get input
+    scanf( "%c", &charInput );
+
+    //print the character
+    printf( "%c", charInput );
+}
