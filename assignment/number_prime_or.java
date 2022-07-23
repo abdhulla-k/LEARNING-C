a397b2a30092224ffe159fe5d2717e2d1c324ab0
abdhulla-k <abdhullak149@gmail.com>
2022-07-23 07:47:36 +0530

    resolved a bug in java program

--- assignment/number_prime_or.java
+++ assignment/number_prime_or.java
@@ -3,20 +3,23 @@ class main{
     public static void main( String[] args ){
         Scanner s = new Scanner( System.in );
         System.out.println( "Enter a number to check is it prime number or not" );
-        int number = s.nextInt();
         boolean flag = false;
-
-        if( number <= 1 ) {
-            flag = false;
-        }
-
-        for( int i = 2; i < number; i++ ) {
-            if( number % i == 0 ) {
+        try {
+            int number = s.nextInt();
+            if( number <= 1 ) {
                 flag = false;
-                break;
-            } else {
-                flag = true;
+
+                for( int i = 2; i < number; i++ ) {
+                    if( number % i == 0 ) {
+                        flag = false;
+                        break;
+                    } else {
+                        flag = true;
+                    }
+                }
             }
+        } finally {
+            s.close();
         }
 
         if( flag == true ) {
